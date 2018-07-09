5781299fa4c6691660068588edef7783397ac086
Mikhail Zholobov <legal90@gmail.com>
2018-07-09 12:56:48 +0200

    Disable auto-provisioning of the master with a test job

--- master.tf
+++ master.tf
@@ -5,7 +5,6 @@ resource "aws_instance" "jmeter-master-instance" {
 
   security_groups = [ "${aws_security_group.jmeter-sg.name}" ]
   key_name = "${aws_key_pair.jmeter-master-keypair.key_name}"
-  iam_instance_profile = "${aws_iam_instance_profile.jmeter_master_iam_profile.name}"
 
   tags {
     Name = "jmeter-master"
@@ -14,33 +13,6 @@ resource "aws_instance" "jmeter-master-instance" {
   connection {
     user = "ec2-user"
     private_key = "${file("${var.master_ssh_private_key_file}")}"
-  }
-
-  provisioner "remote-exec" {
-    inline = [ 
-      "sudo mkdir /jmeter-master",
-      "mkdir ~/.aws",
-      "sudo chown -R ec2-user /jmeter-master",
-      "sudo pip install boto3"
-    ]
-  }
-
-  provisioner "file" {
-    source = "${path.module}/master_start.py"
-    destination = "/jmeter-master/master_start.py"
-  }
-
-  provisioner "file" {
-    source = "${var.jmx_script_file}"
-    destination = "/jmeter-master/script.jmx"
-  }
-
-  provisioner "file" {
-    content = <<EOF
-[default]
-region=${var.aws_region}
-EOF
-    destination = "~/.aws/config"
   }
 
   provisioner "remote-exec" {
@@ -57,34 +29,8 @@ resource "aws_key_pair" "jmeter-master-keypair" {
   public_key = "${file("${var.master_ssh_public_key_file}")}"
 }
 
-resource "aws_iam_instance_profile" "jmeter_master_iam_profile" {
-    name = "jmeter_master_iam_profile"
-    roles = ["${aws_iam_role.jmeter_master_iam_role.name}"]
 }
 
-resource "aws_iam_role" "jmeter_master_iam_role" {
-    name = "jmeter_master_iam_role"
-    path = "/"
-    assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "jmeter_master_iam_role_attachment" {
-    role = "${aws_iam_role.jmeter_master_iam_role.name}"
-    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
-}
 
 output "master_public_ip" {
 	value = "${aws_instance.jmeter-master-instance.public_ip}"
--- slave.tf
+++ slave.tf
@@ -1,35 +1,61 @@
 resource "aws_autoscaling_group" "jmeter-slave-ASG" {
-  availability_zones = ["${split(",", var.availability_zones)}"]
-
-  name = "jmeter-slave-ASG"
-  max_size = "${var.slave_asg_size}"
-  min_size = "${var.slave_asg_size}"
-  force_delete = true
+  name                 = "jmeter-slave-ASG"
+  max_size             = "${var.slave_asg_size}"
+  min_size             = "${var.slave_asg_size}"
+  force_delete         = true
   launch_configuration = "${aws_launch_configuration.jmeter-slave-lc.name}"
+  vpc_zone_identifier  = "${var.subnet_ids}"
 
   tag {
-    key = "Name"
-    value = "jmeter-slave"
+    key                 = "Name"
+    value               = "jmeter-slave"
     propagate_at_launch = "true"
   }
 }
 
 resource "aws_launch_configuration" "jmeter-slave-lc" {
-  name = "jmeter-slave-lc"
-  image_id = "${lookup(var.aws_amis, var.aws_region)}"
+  name_prefix   = "jmeter-slave-lc"
+  image_id      = "${lookup(var.aws_amis, var.aws_region)}"
   instance_type = "${var.slave_instance_type}"
+
   user_data = <<EOF
 #!/bin/sh
-curl ${var.jmeter3_url} > jMeter.tgz
-tar zxvf jMeter.tgz
-apache-jmeter-3.0/bin/jmeter-server
+sudo yum install -y java-1.8.0
+sudo yum remove -y java-1.7.0-openjdk
+sudo mkdir /opt/jmeter
+sudo chown -R ec2-user /opt/jmeter-master
+curl ${var.jmeter3_url} > /tmp/jMeter.tgz
+tar zxvf /tmp/jMeter.tgz -C /opt/jmeter --strip-components=1
+nohup /opt/jmeter/bin/jmeter-server &
 EOF
 
+  # associate_public_ip_address = false
   security_groups = ["${aws_security_group.jmeter-sg.id}"]
-  key_name = "${aws_key_pair.jmeter-slave-keypair.key_name}"
+  key_name        = "${aws_key_pair.jmeter-slave-keypair.key_name}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_key_pair" "jmeter-slave-keypair" {
-  key_name = "jmeter-slave-keypair"
+  key_name   = "jmeter-slave-keypair"
   public_key = "${file("${var.slave_ssh_public_key_file}")}"
+}
+
+# Collect IPs of slaves
+data "aws_instances" "slaves" {
+  instance_tags {
+    Name = "jmeter-slave"
+  }
+
+  depends_on = ["aws_autoscaling_group.jmeter-slave-ASG"]
+}
+
+output "slave_public_ips" {
+  value = "${data.aws_instances.slaves.public_ips}"
+}
+
+output "slave_private_ips" {
+  value = "${data.aws_instances.slaves.private_ips}"
 }
